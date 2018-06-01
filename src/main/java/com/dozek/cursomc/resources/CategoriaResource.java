5dfb1306bf8ae9c09042d4553565cc643f0f352b
Rodrigo <rod.gomes.silva@gmail.com>
2018-06-01 15:01:42 -0300

    Criado categoria banco H2

--- src/main/java/com/dozek/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/dozek/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.dozek.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.dozek.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
+	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Liar Está Funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria(1,"informatica");
+		Categoria cat2 =new Categoria(2,"Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>() ;
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista;
 		
 	}
 
