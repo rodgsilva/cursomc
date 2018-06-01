035c209ff628deb66789fd76ab7800f7de86a6b5
Rodrigo <rod.gomes.silva@gmail.com>
2018-06-01 17:29:01 -0300

    Ajuste no endpont /categoria/id

--- src/main/java/com/dozek/cursomc/services/CategoriaService.java
+++ src/main/java/com/dozek/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.dozek.cursomc.domain.Categoria;
 import com.dozek.cursomc.repositories.CategoriaRepository;
+import com.dozek.cursomc.services.execeptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,6 +16,10 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		
 		Categoria obj=repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! id" + id
+					+",Tipo: "+ Categoria.class.getName());
+		}
 		return obj;
 		
 	}
