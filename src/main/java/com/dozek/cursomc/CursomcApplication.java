0f789de4e0ee180ed74d7904229f5c0f24b0a7ba
Rodrigo <rod.gomes.silva@gmail.com>
2018-06-04 10:18:56 -0300

    Estado e Cidade

--- src/main/java/com/dozek/cursomc/CursomcApplication.java
+++ src/main/java/com/dozek/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.dozek.cursomc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.dozek.cursomc.domain.Categoria;
+import com.dozek.cursomc.domain.Cidade;
+import com.dozek.cursomc.domain.Estado;
 import com.dozek.cursomc.domain.Produto;
 import com.dozek.cursomc.repositories.CategoriaRepository;
+import com.dozek.cursomc.repositories.CidadeRepository;
+import com.dozek.cursomc.repositories.EstadoRepository;
 import com.dozek.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -17,6 +22,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository repo;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	@Autowired
 	private ProdutoRepository prodRepo;
@@ -42,9 +53,22 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategoria().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategoria().addAll(Arrays.asList(cat1));
 		
-		
+				
 		repo.save(Arrays.asList(cat1,cat2));
 		prodRepo.save(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null,"Minas Gerais");
+		Estado est2 = new Estado(null,"São Paulo");
+		
+		Cidade c1 = new Cidade(null,"Uberlandia",est1);
+		Cidade c2 = new Cidade(null,"São Paulo", est2);
+		Cidade c3 = new Cidade(null,"Campinas",est2);
+		
+		est1.getCidade().addAll(Arrays.asList(c1));
+		est2.getCidade().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.save(Arrays.asList(est1,est2));
+		cidadeRepository.save(Arrays.asList(c1,c2,c3));
 		
 	}
 	
---
+++ src/main/java/com/dozek/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.dozek.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dozek.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade,Integer> {
+
+}
