ab2db928ab46d33345ddeeb4e04c92f69de85e56
Rodrigo <rod.gomes.silva@gmail.com>
2018-06-05 16:03:29 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/dozek/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.dozek.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dozek.cursomc.domain.Pedido;
+import com.dozek.cursomc.repositories.PedidoRepository;
+import com.dozek.cursomc.services.execeptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		
+		Pedido obj=repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! id" + id
+					+", Tipo: "+ Pedido.class.getName());
+		}
+		return obj;
+		
+	}
+
+}
