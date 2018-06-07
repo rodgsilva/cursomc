a672b9b9998348e03fd47659c3598c745d81583e
Rodrigo <rod.gomes.silva@gmail.com>
2018-06-07 14:51:20 -0300

    PUT,DELETE E GET para Cleinte

--- src/main/java/com/dozek/cursomc/services/ClienteService.java
+++ src/main/java/com/dozek/cursomc/services/ClienteService.java
@@ -1,10 +1,18 @@
 package com.dozek.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.dozek.cursomc.domain.Cliente;
+import com.dozek.cursomc.domain.dto.ClienteDTO;
 import com.dozek.cursomc.repositories.ClienteRepository;
+import com.dozek.cursomc.services.execeptions.DataIntegrityException;
 import com.dozek.cursomc.services.execeptions.ObjectNotFoundException;
 
 @Service
@@ -23,5 +31,40 @@ public class ClienteService {
 		return obj;
 		
 	}
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+			}
+			catch(DataIntegrityViolationException e) {
+				throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			}
+	}
+	
+	public List<Cliente>findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page,Integer linesPerPage, String ordeBy,String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage,Direction.valueOf(direction),ordeBy);
+		return repo.findAll(pageRequest);		
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+		
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
 
 }
