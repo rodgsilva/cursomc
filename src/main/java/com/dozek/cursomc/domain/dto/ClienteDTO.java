b94c68dbfa0546f5d408117f69b254aa56159d71
Rodrigo <rod.gomes.silva@gmail.com>
2018-06-08 13:23:48 -0300

    Validação customixasa: email nãp repetido na atualização de cliente

--- src/main/java/com/dozek/cursomc/domain/dto/ClienteDTO.java
+++ src/main/java/com/dozek/cursomc/domain/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.dozek.cursomc.domain.Cliente;
+import com.dozek.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
