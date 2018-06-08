830a3420209465132af2b8cb211ba58b5dcad305
Rodrigo <rod.gomes.silva@gmail.com>
2018-06-08 18:27:49 -0300

    Busca de prosutos por nome e categorias

--- src/main/java/com/dozek/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/dozek/cursomc/repositories/ProdutoRepository.java
@@ -1,11 +1,22 @@
 package com.dozek.cursomc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.dozek.cursomc.domain.Categoria;
 import com.dozek.cursomc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
+	
+	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categoria cat WHERE obj.nome LIKE %:nome% AND cat IN :categoria ")
+	//Page<Produto> search(@Param("nome") String nome, @Param("categoria")List<Categoria> categoria,Pageable pageRequest);
 
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriaIn(String nome,List<Categoria> categoria,Pageable pageRequest);
 }
